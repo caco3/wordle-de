6f68b0aa6f8ac575db45184087f4ed1685e78b49
CaCO3 <caco@ruinelli.ch>
2022-02-25 23:36:53 +0100

    add server side log

--- get-word-of-the-day.php
+++ get-word-of-the-day.php
@@ -1,6 +1,15 @@
 <?php
 
+    $LOG_FOLDER = "./logs";
+
     if (isset($_GET['timestamp'])) {
+        if (!is_dir($LOG_FOLDER)) {
+            mkdir($LOG_FOLDER);
+        }
+
+        $log  = date("[Y-m-d, H:i:s] ") . $_GET['timestamp'] . ", " . $_SERVER['REMOTE_ADDR'] . ", " . $_SERVER['HTTP_USER_AGENT'] . PHP_EOL;
+        file_put_contents('./logs/word-of-the-day.log', $log, FILE_APPEND);
+
         $words = json_decode(file_get_contents("target-words.json"), true)["data"];
 
         srand($_GET['timestamp']);
